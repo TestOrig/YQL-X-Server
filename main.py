4dbbd8fff544a65fa42da4e8684677db2ca78dc0
electimon <electimon@gmail.com>
2022-12-18 12:13:15 -0500

    error handling in main

--- main.py
+++ main.py
@@ -21,35 +21,37 @@ def legacyyql():
 @app.route('/yql/weather/dgw', methods=["POST"])
 def legacydgw():
     print("Pre iOS 5 app found!")
-    sentXML = request.data.decode()
-    reqType = sentXML[sentXML.index("y id=")+5:sentXML.index(" time")].replace('"', "")
-    print(reqType)
-    if reqType == "3":
-        q = sentXML[sentXML.index("ase>")+4:sentXML.index("</phr")]
-        if "|" in q:
-            q = q.split("|")[1]
-        return XMLGenerator.getXMLforSearchWithYQLLegacy(yql, q)
-    if reqType == "30":
-        q = sentXML[sentXML.index("id>")+3:sentXML.index("</id")]
-        if "|" in q:
-            q = q.split("|")[1]
-        print("q = " + q)
-        return XMLGenerator.getXMLforWeatherWithYQLLegacy(yql, q)
-    return ""
+    if request.data:
+        sentXML = request.data.decode()
+        reqType = sentXML[sentXML.index("y id=")+5:sentXML.index(" time")].replace('"', "")
+        print(reqType)
+        if reqType == "3":
+            q = sentXML[sentXML.index("ase>")+4:sentXML.index("</phr")]
+            if "|" in q:
+                q = q.split("|")[1]
+            return XMLGenerator.getXMLforSearchWithYQLLegacy(yql, q)
+        if reqType == "30":
+            q = sentXML[sentXML.index("id>")+3:sentXML.index("</id")]
+            if "|" in q:
+                q = q.split("|")[1]
+            print("q = " + q)
+            return XMLGenerator.getXMLforWeatherWithYQLLegacy(yql, q)
+        return ""
 
 # iOS 6 contacts this endpoint for all things weather
 @app.route('/yql/weather')
 def weatherEndpoint():
     # Get the request and set the request for the yql handling object
     q = request.args.get('q')
-    print(q)
-    if 'partner.weather.locations' and not 'yql.query.multi' in q:
-        # Search Request
-        q = q[q.index('query="')+7:q.index('" a')]
-        return searchReq(q)
-    elif 'partner.weather.forecasts' in q:
-        # Weather Request
-        return weatherReq(q)
+    if q:
+        print(q)
+        if 'partner.weather.locations' and not 'yql.query.multi' in q:
+            # Search Request
+            q = q[q.index('query="')+7:q.index('" a')]
+            return searchReq(q)
+        elif 'partner.weather.forecasts' in q:
+            # Weather Request
+            return weatherReq(q)
     
 def searchReq(q):
     return XMLGenerator.getXMLforSearchWithYQL(yql, q)
